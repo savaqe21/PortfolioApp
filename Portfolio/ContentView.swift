c251d81fd5f607d8c1d7070ee59aa0e4ef78be42
savaqe21 <fabianlesniak@icloud.com>
2023-10-14 21:50:04 +0200

    Adding NavigationView

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -9,9 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
+        NavigationStack {
             List {
-                NavigationLink("Fabian Leśniak", destination: AboutMeView())
+                Section{
+                    NavigationLink(destination: AboutMeView()){
+                        HStack{
+                            Image("savaqe21-logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100, alignment: .center)
+                            VStack{
+                                Text("Fabian Leśniak")
+                                    .font(.headline)
+                                Text("A little about me")
+                                    .font(.callout)
+                                    .foregroundColor(.element)
+                            }
+                        }
+                    }
+                }
+                // do przerobienia na foreach
+                Section(header: Text("Mine apps")) {
+                    NavigationLink(destination: AboutMeView()){
+                        HStack{
+                            Image("iExpense-logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                            VStack{
+                                Text("iExpense")
+                                    .font(.subheadline)
+                            }
+                        }
+                    }
+                    
+                    NavigationLink(destination: AboutMeView()){
+                        HStack{
+                            Image("BetterRest-logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                            VStack{
+                                Text("BetterRest")
+                                    .font(.subheadline)
+                            }
+                        }
+                    }
+                }
+                .headerProminence(.increased)
             }
             .navigationTitle("Portfolio")
         }
